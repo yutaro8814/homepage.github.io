93d5b9db28a28cad4888bb040e21c5ad318ef394
yutaro8814 <y.o.814@icloud.com>
2022-01-24 03:40:26 +0900

    ver2

---
+++ scripts/main.js
@@ -0,0 +1,55 @@
+const headerBtn = document.querySelector('.header-btn');
+const globalContainer = document.querySelector('#global-container');
+
+headerBtn.addEventListener('click',navigationOpen);
+headerBtn.addEventListener('click',navigationOpen);
+
+
+function navigationOpen(){
+    globalContainer.classList.toggle('navigation-open');
+}
+
+// Params
+var sliderSelector = '.swiper-container',
+    options = {
+      init: false,
+      loop: true,
+      speed:800,
+      slidesPerView: 2, // or 'auto'
+      // spaceBetween: 10,
+      centeredSlides : true,
+      effect: 'coverflow', // 'cube', 'fade', 'coverflow',
+      coverflowEffect: {
+        rotate: 50, // Slide rotate in degrees
+        stretch: 0, // Stretch space between slides (in px)
+        depth: 100, // Depth offset in px (slides translate in Z axis)
+        modifier: 1, // Effect multipler
+        slideShadows : true, // Enables slides shadows
+      },
+      grabCursor: true,
+      parallax: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+          spaceBetween: 0
+        }
+      },
+      // Events
+      on: {
+        imagesReady: function(){
+          this.el.classList.remove('loading');
+        }
+      }
+    };
+let mySwiper = new Swiper(sliderSelector, options);
+
+// Initialize slider
+mySwiper.init();
